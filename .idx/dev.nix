730c6d2c21334d31bf5f143640ce3cdcb408c253
lee <lee@start341.com>
2025-05-25 22:53:27 +0000

    shell.nix updated to nodepackages

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,40 +1,10 @@
-{ pkgs, ... }:
+{ pkgs ? import <nixpkgs> {} }:
 
-{
-  channel = "stable-24.05";
-
-  packages = with pkgs; [
+pkgs.mkShell {
+  buildInputs = with pkgs; [
     nodejs_20
-    nodePackages.pnpm
-    nodePackages.firebase-tools
-    typescript
-    vite
-    git
-    direnv
-  ];
-
-  env = {
-    NODE_ENV = "development";
-  };
-
-  idx.previews = {
-    enable = true;
-    previews = [
-      {
-        id      = "web";
-        cwd     = "client";
-        command = [
-          "pnpm"
-          "run"
-          "dev"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-        ];
-        manager = "web";
-      }
-    ];
-  };
+ nodePackages.pnpm
+ firebase-tools
+ typescript
+ ];
 }
--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs_20
-    pnpm
+    nodePackages.pnpm
     firebase-tools
     typescript
   ];
